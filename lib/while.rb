715e20fe37240e43065f6b02e8f5ab87045d6c19
Woodelin Florveus <woodelinflorveus@gmail.com>
2020-09-30 14:55:09 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,8 +1,8 @@
 def using_while
   levitation_force = 0
 
-  while levitation_force < 10
-    levitation_force +=10
+  while levitation_force < 4
+    levitation_force +=1
     puts "Wingardium Leviosa"
   end
 
